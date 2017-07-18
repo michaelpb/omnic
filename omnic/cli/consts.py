ef53198be35b8d5bc52af2ce9054cb2de0afb9f9
michaelb <michaelpb@gmail.com>
2017-07-18 15:14:21 -0700

    adding scaffolding command

---
+++ omnic/cli/consts.py
@@ -0,0 +1,36 @@
+SETTINGS_PY = '''
+##### Web server settings
+
+HOST = '127.0.0.1'
+PORT = 8080
+DEBUG = True
+
+
+##### Security settings
+
+# Whitelist allowed foreign hosts. This should point to your app servers, your
+# CDN, or cloud hosting such as your S3 buckets.
+ALLOWED_LOCATIONS = {
+    'localhost', '127.0.0.1',
+    # Example: 'media.mysite.com', 's3-media.mysite.com'
+}
+
+
+##### Conversion settings
+
+# Conversion graph settings
+CONVERSION_PROFILES = {
+    # Add in profiles here, like the following:
+    # 'media-gallery-thumb.png': ('PNG', 'thumb.png:300x300'),
+}
+
+# Where to store cache
+PATH_PREFIX = '/var/tmp/omnic/'
+'''
+
+
+CLI_HELP_EPILOG_TEMPLATE ='''
+
+available subcommands:
+%s
+'''
