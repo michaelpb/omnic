13f5d69c9f189ac9725ac9551a0e1bfd3f6af78a
michaelb <michaelpb@gmail.com>
2017-07-19 12:41:35 -0700

    fixing annoying mistake re. find packages

--- omnic/__init__.py
+++ omnic/__init__.py
@@ -2,12 +2,7 @@ __author__ = 'michaelb'
 __email__ = 'michaelpb@gmail.com'
 __version__ = '0.1.6'
 
-
 # flake8: noqa
-# Import all top-level packages
-from . import (
-    builtin, config, cli, conversion, responses, types, utils, web, worker,
-)
 
 # Set up singleton system
 from omnic.utils.singleton import SingletonManager
