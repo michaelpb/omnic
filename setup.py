df14b216e21813e2ed607a70d1c5ff53f8c940d3
michaelb <michaelpb@gmail.com>
2017-10-10 14:24:31 -0700

    Bump version: 0.1.15 → 0.1.16

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ readme = open('README.rst').read()
 
 setup(
     name='omnic',
-    version='0.1.15',
+    version='0.1.16',
     description='Mostly stateless microservice framework for generating '
     'on-the-fly thumbs and previews of a wide variety of file types.',
     long_description=readme,
